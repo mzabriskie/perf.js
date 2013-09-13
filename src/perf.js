1fd1c201bc2c4b6de0332419d176d3dd1e2a6980
mzabriskie <mzabriskie@gmail.com>
2013-09-12 22:40:28 -0600

    Limit the number of frames to render

--- src/perf.js
+++ src/perf.js
@@ -6,7 +6,8 @@
     (function (perf) {
         /**
          * Format duration (e.g., 37ms, 22m:45s:13ms, etc.)
-         * @param {Number} millis
+         *
+         * @param {Number} millis The duration in milliseconds to be formatted
          * @returns {String}
          */
         function duration(millis) {
@@ -35,8 +36,9 @@
 
         /**
          * Measure how long a function takes to execute
-         * @param {Function} target
-         * @param {Function} callback
+         *
+         * @param {Function} target The function that you would like to measure performance for
+         * @param {Function} callback The function that is called once performance has been measured
          */
         perf.time = function (target, callback) {
             var now = Date.now();
@@ -57,13 +59,16 @@
         /**
          * Measure how many frames per second a function executes
          * Based on http://stackoverflow.com/questions/5078913/html5-canvas-performance-calculating-loops-frames-per-second
-         * @param {Function} target
-         * @param {Function} callback
+         *
+         * @param {Function} target The function that will perform rendering
+         * @param {Function} callback The function that is called when FPS is updated
+         * @param {Number} [limit] The total number of times to run <code>target</code>
          */
-        perf.fps = function (target, callback) {
-            var fps = 0, now, last = Date.now() - 1;
+        perf.fps = function (target, callback, limit) {
+            var fps = 0, count = 0, timer,
+                now, last = Date.now() - 1;
 
-            setInterval(function () {
+            timer = setInterval(function () {
                 target.apply(this, arguments);
 
                 var frame = 1000 / ((now = Date.now()) - last),
@@ -75,28 +80,36 @@
                     fps = sample;
                 }
                 last = now;
+
+                if (typeof limit !== 'undefined' && ++count >= limit) {
+                    clearInterval(timer);
+                    callback.call(this, fps, fps.toFixed(1) + 'fps');
+                }
             }, 1);
 
-            callback.call(this, fps, fps.toFixed(1) + 'fps');
-            setInterval(function () { callback.call(this, fps, fps.toFixed(1) + 'fps'); }, 1000);
+            if (typeof limit === 'undefined') {
+                callback.call(this, fps, fps.toFixed(1) + 'fps');
+                setInterval(function () { callback.call(this, fps, fps.toFixed(1) + 'fps'); }, 1000);
+            }
         };
     })(perf);
 
     // perf.bandwidth
     (function (perf) {
         var BITS = 8, // Bits in a byte
-            IMAGE_SIZE = 2095282 * BITS; // Bit size of download
+            SIZE = 2095282 * BITS; // Bit size of download
 
         /**
          * Measure bit-rate of network connection
-         * @param {Function} callback
+         *
+         * @param {Function} callback The function that is called once bandwidth has been measured
          */
         perf.bandwidth = function (callback) {
             var img = new Image(), now;
 
             img.onload = function () {
                 var diff = (Date.now() - now) / 1000;
-                callback.call(this, IMAGE_SIZE / diff);
+                callback.call(this, SIZE / diff);
             };
 
             now = Date.now();
